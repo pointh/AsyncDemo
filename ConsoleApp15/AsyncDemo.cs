bc0f61c2442acfefdf28faf706cb33291e7d3897
Pavel Svec <pavel.svec@itcrawl.cz>
2020-12-08 20:09:31 +0100

    Async a nečekej

--- ConsoleApp15/AsyncDemo.cs
+++ ConsoleApp15/AsyncDemo.cs
@@ -8,42 +8,62 @@ namespace AsyncDemoNS
 {
     class AsyncDemo
     {
-        public static void MocPrace()
+        private static readonly object consoleLock = new object();
+
+        public async static Task<int> MocPrace()
         {
-            for (int i = 0; i < 100; i++)
+            int i = 0;
+            await Task.Run(() =>
             {
-                System.Threading.Thread.Sleep(1000);
-                Console.SetCursorPosition(10, 10);
-                Console.Write(i);
-            }
+                for (i = 0; i < 20; i++)
+                {
+                    Debug.WriteLine($"MocPrace:{System.Threading.Thread.CurrentThread.ManagedThreadId}", "SYNC");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.SetCursorPosition(10, 10);
+                    Console.Write(i);
+                    Console.SetCursorPosition(0, 0);
+                }
+            });
+            return i;
         }
 
-        static void Main(string[] args)
+        public async static Task Vrtule(int x, int y)
         {
-            Console.WriteLine("1 problém");
-            Console.SetCursorPosition(0, 0);
+            void OverwriteOn0(char c)
+            {
+                lock (consoleLock)
+                {
+                    System.Threading.Thread.Sleep(50);
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(c);
+                }
+            }
+
+            await Task.Run(() =>
+            {
+                Console.CursorVisible = false;
+
+                while (true)
+                {
+                    Debug.WriteLine($"Vrtule[{x},{y}]: {System.Threading.Thread.CurrentThread.ManagedThreadId}", "SYNC");
+                    OverwriteOn0('|');
+                    OverwriteOn0('/');
+                    OverwriteOn0('-');
+                    OverwriteOn0('\\');
+                }
+            });
+        }
+
+        static async Task Main(string[] args)
+        {
+            Debug.WriteLine($"Main:{System.Threading.Thread.CurrentThread.ManagedThreadId}", "SYNC");
 
             MocPrace();
 
-            Console.CursorVisible = false;
-            while (true)
-            {
-                Console.Write('|');
-                Console.SetCursorPosition(0, 0);
-                System.Threading.Thread.Sleep(500);
+            Vrtule(0, 0);
 
-                Console.Write('/');
-                Console.SetCursorPosition(0, 0);
-                System.Threading.Thread.Sleep(500);
+            await Vrtule(5, 0);
 
-                Console.Write('-');
-                Console.SetCursorPosition(0, 0);
-                System.Threading.Thread.Sleep(500);
-
-                Console.Write('\\');
-                Console.SetCursorPosition(0, 0);
-                System.Threading.Thread.Sleep(500);
-            }
         }
     }
 }
