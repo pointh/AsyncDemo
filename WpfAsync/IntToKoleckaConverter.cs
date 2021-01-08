a3710209633b872d24c337c0dc6d891e6cfd4482
Pavel Svec <pavel.svec@itcrawl.cz>
2021-01-08 15:47:37 +0100

    S IValueConverter

---
+++ WpfAsync/IntToKoleckaConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfAsync
+{
+    class IntToKoleckaConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int i = (int)value;
+            return $"Ještě {i} koleček";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
